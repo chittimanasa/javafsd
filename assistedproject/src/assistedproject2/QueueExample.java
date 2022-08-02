7a1024b44824dd43573d83df13f35a034c3434fd
chittimanasa <chittimanasa29@gmail.com>
2022-08-02 15:02:42 +0530

    assistedproject2

---
+++ assistedproject/src/assistedproject2/QueueExample.java
@@ -0,0 +1,21 @@
+package assistedproject2;
+
+import java.util.*;
+import java.util.LinkedList;
+
+public class QueueExample {
+	public static void main(String[] args) 
+	{
+	        		Queue<String> locationsQueue = new LinkedList<>();
+	                locationsQueue.add("Banglore");
+	        		locationsQueue.add("chennai");
+	        		locationsQueue.add("hyderabad");
+	        		locationsQueue.add("america");
+	                System.out.println("Queue is : " + locationsQueue);
+	        		System.out.println("Head of Queue : " + locationsQueue.peek());
+	        		locationsQueue.remove();
+	        		System.out.println("After removing Head of Queue : " + locationsQueue);
+	        		System.out.println("Size of Queue : " + locationsQueue.size());
+	    	}
+	
+}
---
+++ assistedproject/src/assistedproject2/updatefile.java
@@ -0,0 +1,21 @@
+package assistedproject2;
+
+import java.io.FileWriter;   
+import java.io.IOException;   
+
+public class updatefile {
+	
+	public static void main(String[] args) {  
+	      
+	    try {  
+	        FileWriter fwrite = new FileWriter("D:FileOperationCreate.txt"); 
+	        fwrite.write("A named location used to store related information is referred to as a File.");   
+	
+	        fwrite.close();   
+	        System.out.println("Content is successfully wrote to the file.");  
+	    } catch (IOException e) {  
+	        System.out.println("Unexpected error occurred");  
+	        e.printStackTrace();  
+	        }  
+	    }  
+}
